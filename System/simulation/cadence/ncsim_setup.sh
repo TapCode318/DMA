b3c289de0961ab23ad8f8883a186a2851fe20f29
TapCode318 <levanloi1357913579@gmail.com>
2026-05-10 23:44:14 +0700

    Add files via upload

---
+++ System/simulation/cadence/ncsim_setup.sh
@@ -0,0 +1,109 @@
+
+# (C) 2001-2018 Altera Corporation. All rights reserved.
+# Your use of Altera Corporation's design tools, logic functions and 
+# other software and tools, and its AMPP partner logic functions, and 
+# any output files any of the foregoing (including device programming 
+# or simulation files), and any associated documentation or information 
+# are expressly subject to the terms and conditions of the Altera 
+# Program License Subscription Agreement, Altera MegaCore Function 
+# License Agreement, or other applicable license agreement, including, 
+# without limitation, that your use is for the sole purpose of 
+# programming logic devices manufactured by Altera and sold by Altera 
+# or its authorized distributors. Please refer to the applicable 
+# agreement for further details.
+
+# ACDS 13.0 156 win32 2018.05.10.08:37:33
+
+# ----------------------------------------
+# ncsim - auto-generated simulation script
+
+# ----------------------------------------
+# initialize variables
+TOP_LEVEL_NAME="System"
+QSYS_SIMDIR="./../"
+QUARTUS_INSTALL_DIR="C:/altera/13.0/quartus/"
+SKIP_FILE_COPY=0
+SKIP_DEV_COM=0
+SKIP_COM=0
+SKIP_ELAB=0
+SKIP_SIM=0
+USER_DEFINED_ELAB_OPTIONS=""
+USER_DEFINED_SIM_OPTIONS="-input \"@run 100; exit\""
+
+# ----------------------------------------
+# overwrite variables - DO NOT MODIFY!
+# This block evaluates each command line argument, typically used for 
+# overwriting variables. An example usage:
+#   sh <simulator>_setup.sh SKIP_ELAB=1 SKIP_SIM=1
+for expression in "$@"; do
+  eval $expression
+  if [ $? -ne 0 ]; then
+    echo "Error: This command line argument, \"$expression\", is/has an invalid expression." >&2
+    exit $?
+  fi
+done
+
+# ----------------------------------------
+# create compilation libraries
+mkdir -p ./libraries/work/
+mkdir -p ./libraries/jtag_uart_0/
+mkdir -p ./libraries/nios2_qsys_0/
+mkdir -p ./libraries/onchip_memory2_0/
+mkdir -p ./libraries/altera_ver/
+mkdir -p ./libraries/lpm_ver/
+mkdir -p ./libraries/sgate_ver/
+mkdir -p ./libraries/altera_mf_ver/
+mkdir -p ./libraries/altera_lnsim_ver/
+mkdir -p ./libraries/cycloneii_ver/
+
+# ----------------------------------------
+# copy RAM/ROM files to simulation directory
+if [ $SKIP_FILE_COPY -eq 0 ]; then
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_ociram_default_contents.dat ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_ociram_default_contents.hex ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_ociram_default_contents.mif ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_a.dat ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_a.hex ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_a.mif ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_b.dat ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_b.hex ./
+  cp -f $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_b.mif ./
+  cp -f $QSYS_SIMDIR/submodules/System_onchip_memory2_0.hex ./
+fi
+
+# ----------------------------------------
+# compile device library files
+if [ $SKIP_DEV_COM -eq 0 ]; then
+  ncvlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/altera_primitives.v" -work altera_ver      
+  ncvlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/220model.v"          -work lpm_ver         
+  ncvlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/sgate.v"             -work sgate_ver       
+  ncvlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/altera_mf.v"         -work altera_mf_ver   
+  ncvlog -sv "$QUARTUS_INSTALL_DIR/eda/sim_lib/altera_lnsim.sv"     -work altera_lnsim_ver
+  ncvlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/cycloneii_atoms.v"   -work cycloneii_ver   
+fi
+
+# ----------------------------------------
+# compile design files in correct order
+if [ $SKIP_COM -eq 0 ]; then
+  ncvlog "$QSYS_SIMDIR/submodules/System_jtag_uart_0.v"                            -work jtag_uart_0      -cdslib ./cds_libs/jtag_uart_0.cds.lib     
+  ncvlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0.v"                           -work nios2_qsys_0     -cdslib ./cds_libs/nios2_qsys_0.cds.lib    
+  ncvlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_jtag_debug_module_sysclk.v"  -work nios2_qsys_0     -cdslib ./cds_libs/nios2_qsys_0.cds.lib    
+  ncvlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_jtag_debug_module_tck.v"     -work nios2_qsys_0     -cdslib ./cds_libs/nios2_qsys_0.cds.lib    
+  ncvlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_jtag_debug_module_wrapper.v" -work nios2_qsys_0     -cdslib ./cds_libs/nios2_qsys_0.cds.lib    
+  ncvlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_oci_test_bench.v"            -work nios2_qsys_0     -cdslib ./cds_libs/nios2_qsys_0.cds.lib    
+  ncvlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_test_bench.v"                -work nios2_qsys_0     -cdslib ./cds_libs/nios2_qsys_0.cds.lib    
+  ncvlog "$QSYS_SIMDIR/submodules/System_onchip_memory2_0.v"                       -work onchip_memory2_0 -cdslib ./cds_libs/onchip_memory2_0.cds.lib
+  ncvlog "$QSYS_SIMDIR/System.v"                                                                                                                     
+fi
+
+# ----------------------------------------
+# elaborate top level design
+if [ $SKIP_ELAB -eq 0 ]; then
+  ncelab -access +w+r+c -namemap_mixgen $USER_DEFINED_ELAB_OPTIONS $TOP_LEVEL_NAME
+fi
+
+# ----------------------------------------
+# simulate
+if [ $SKIP_SIM -eq 0 ]; then
+  eval ncsim -licqueue $USER_DEFINED_SIM_OPTIONS $TOP_LEVEL_NAME
+fi
---
+++ System/simulation/mentor/msim_setup.tcl
@@ -0,0 +1,176 @@
+
+# (C) 2001-2018 Altera Corporation. All rights reserved.
+# Your use of Altera Corporation's design tools, logic functions and 
+# other software and tools, and its AMPP partner logic functions, and 
+# any output files any of the foregoing (including device programming 
+# or simulation files), and any associated documentation or information 
+# are expressly subject to the terms and conditions of the Altera 
+# Program License Subscription Agreement, Altera MegaCore Function 
+# License Agreement, or other applicable license agreement, including, 
+# without limitation, that your use is for the sole purpose of 
+# programming logic devices manufactured by Altera and sold by Altera 
+# or its authorized distributors. Please refer to the applicable 
+# agreement for further details.
+
+# ACDS 13.0 156 win32 2018.05.10.08:37:33
+
+# ----------------------------------------
+# Auto-generated simulation script
+
+# ----------------------------------------
+# Initialize the variable
+if ![info exists SYSTEM_INSTANCE_NAME] { 
+  set SYSTEM_INSTANCE_NAME ""
+} elseif { ![ string match "" $SYSTEM_INSTANCE_NAME ] } { 
+  set SYSTEM_INSTANCE_NAME "/$SYSTEM_INSTANCE_NAME"
+} 
+
+if ![info exists TOP_LEVEL_NAME] { 
+  set TOP_LEVEL_NAME "System"
+} 
+
+if ![info exists QSYS_SIMDIR] { 
+  set QSYS_SIMDIR "./../"
+} 
+
+if ![info exists QUARTUS_INSTALL_DIR] { 
+  set QUARTUS_INSTALL_DIR "C:/altera/13.0/quartus/"
+} 
+
+
+# ----------------------------------------
+# Copy ROM/RAM files to simulation directory
+alias file_copy {
+  echo "\[exec\] file_copy"
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_ociram_default_contents.dat ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_ociram_default_contents.hex ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_ociram_default_contents.mif ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_a.dat ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_a.hex ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_a.mif ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_b.dat ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_b.hex ./
+  file copy -force $QSYS_SIMDIR/submodules/System_nios2_qsys_0_rf_ram_b.mif ./
+  file copy -force $QSYS_SIMDIR/submodules/System_onchip_memory2_0.hex ./
+}
+
+# ----------------------------------------
+# Create compilation libraries
+proc ensure_lib { lib } { if ![file isdirectory $lib] { vlib $lib } }
+ensure_lib          ./libraries/     
+ensure_lib          ./libraries/work/
+vmap       work     ./libraries/work/
+vmap       work_lib ./libraries/work/
+if { ![ string match "*ModelSim ALTERA*" [ vsim -version ] ] } {
+  ensure_lib                  ./libraries/altera_ver/      
+  vmap       altera_ver       ./libraries/altera_ver/      
+  ensure_lib                  ./libraries/lpm_ver/         
+  vmap       lpm_ver          ./libraries/lpm_ver/         
+  ensure_lib                  ./libraries/sgate_ver/       
+  vmap       sgate_ver        ./libraries/sgate_ver/       
+  ensure_lib                  ./libraries/altera_mf_ver/   
+  vmap       altera_mf_ver    ./libraries/altera_mf_ver/   
+  ensure_lib                  ./libraries/altera_lnsim_ver/
+  vmap       altera_lnsim_ver ./libraries/altera_lnsim_ver/
+  ensure_lib                  ./libraries/cycloneii_ver/   
+  vmap       cycloneii_ver    ./libraries/cycloneii_ver/   
+}
+ensure_lib                  ./libraries/jtag_uart_0/     
+vmap       jtag_uart_0      ./libraries/jtag_uart_0/     
+ensure_lib                  ./libraries/nios2_qsys_0/    
+vmap       nios2_qsys_0     ./libraries/nios2_qsys_0/    
+ensure_lib                  ./libraries/onchip_memory2_0/
+vmap       onchip_memory2_0 ./libraries/onchip_memory2_0/
+
+# ----------------------------------------
+# Compile device library files
+alias dev_com {
+  echo "\[exec\] dev_com"
+  if { ![ string match "*ModelSim ALTERA*" [ vsim -version ] ] } {
+    vlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/altera_primitives.v" -work altera_ver      
+    vlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/220model.v"          -work lpm_ver         
+    vlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/sgate.v"             -work sgate_ver       
+    vlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/altera_mf.v"         -work altera_mf_ver   
+    vlog -sv "$QUARTUS_INSTALL_DIR/eda/sim_lib/altera_lnsim.sv"     -work altera_lnsim_ver
+    vlog     "$QUARTUS_INSTALL_DIR/eda/sim_lib/cycloneii_atoms.v"   -work cycloneii_ver   
+  }
+}
+
+# ----------------------------------------
+# Compile the design files in correct order
+alias com {
+  echo "\[exec\] com"
+  vlog "$QSYS_SIMDIR/submodules/System_jtag_uart_0.v"                            -work jtag_uart_0     
+  vlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0.v"                           -work nios2_qsys_0    
+  vlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_jtag_debug_module_sysclk.v"  -work nios2_qsys_0    
+  vlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_jtag_debug_module_tck.v"     -work nios2_qsys_0    
+  vlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_jtag_debug_module_wrapper.v" -work nios2_qsys_0    
+  vlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_oci_test_bench.v"            -work nios2_qsys_0    
+  vlog "$QSYS_SIMDIR/submodules/System_nios2_qsys_0_test_bench.v"                -work nios2_qsys_0    
+  vlog "$QSYS_SIMDIR/submodules/System_onchip_memory2_0.v"                       -work onchip_memory2_0
+  vlog "$QSYS_SIMDIR/System.v"                                                                         
+}
+
+# ----------------------------------------
+# Elaborate top level design
+alias elab {
+  echo "\[exec\] elab"
+  vsim -t ps -L work -L work_lib -L jtag_uart_0 -L nios2_qsys_0 -L onchip_memory2_0 -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cycloneii_ver $TOP_LEVEL_NAME
+}
+
+# ----------------------------------------
+# Elaborate the top level design with novopt option
+alias elab_debug {
+  echo "\[exec\] elab_debug"
+  vsim -novopt -t ps -L work -L work_lib -L jtag_uart_0 -L nios2_qsys_0 -L onchip_memory2_0 -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cycloneii_ver $TOP_LEVEL_NAME
+}
+
+# ----------------------------------------
+# Compile all the design files and elaborate the top level design
+alias ld "
+  dev_com
+  com
+  elab
+"
+
+# ----------------------------------------
+# Compile all the design files and elaborate the top level design with -novopt
+alias ld_debug "
+  dev_com
+  com
+  elab_debug
+"
+
+# ----------------------------------------
+# Print out user commmand line aliases
+alias h {
+  echo "List Of Command Line Aliases"
+  echo
+  echo "file_copy                     -- Copy ROM/RAM files to simulation directory"
+  echo
+  echo "dev_com                       -- Compile device library files"
+  echo
+  echo "com                           -- Compile the design files in correct order"
+  echo
+  echo "elab                          -- Elaborate top level design"
+  echo
+  echo "elab_debug                    -- Elaborate the top level design with novopt option"
+  echo
+  echo "ld                            -- Compile all the design files and elaborate the top level design"
+  echo
+  echo "ld_debug                      -- Compile all the design files and elaborate the top level design with -novopt"
+  echo
+  echo 
+  echo
+  echo "List Of Variables"
+  echo
+  echo "TOP_LEVEL_NAME                -- Top level module name."
+  echo
+  echo "SYSTEM_INSTANCE_NAME          -- Instantiated system module name inside top level module."
+  echo
+  echo "QSYS_SIMDIR                   -- Qsys base simulation directory."
+  echo
+  echo "QUARTUS_INSTALL_DIR           -- Quartus installation directory."
+}
+file_copy
+h
